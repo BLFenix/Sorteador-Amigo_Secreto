f3566633801d46e99cfe971403f8caa8b444d3ee
BLFenix <gabrielramoscastro761@gmail.com>
2025-02-10 10:19:05 -0300

    Adicionando a função remover amigo e o mínimo de pessoas para sorteio

--- js/app.js
+++ js/app.js
@@ -6,40 +6,55 @@ function adicionar(){
 
     if (amigo == "") {
         alert("Erro! Nome nulo!");
-        
     }else {
         if (Amigos.includes(amigo)) {
         alert("Erro! Nome já cadastrado!");
         }else{
-            let listAmigos = document.getElementById("lista-amigos");
-        
-            if (listAmigos.textContent == "") {
-                listAmigos.textContent = amigo;
-            }else {
-                listAmigos.textContent += `, ${amigo}`;
-            }
-
             Amigos.push(amigo);
-
-            (document.getElementById("nome-amigo")).value = "";
+            alert(Amigos.length);
+            modificarLista();
         }
     }
+}
+
+function modificarLista(){
+    let listAmigos = document.getElementById("lista-amigos");
+    listAmigos.innerHTML = ""; 
+
+    for (let i = 0; i < Amigos.length; i++) {
+        if (i == 0) {
+            listAmigos.innerHTML = `<a onclick="removerAmigo(this.innerText)" class="amigos_nome">${Amigos[i]}</a>`;
+        }else {
+            listAmigos.innerHTML += `, <a onclick="removerAmigo(this.innerText)" class="amigos_nome">${Amigos[i]}</a>`;
+        }
+    }
+    
+    (document.getElementById("nome-amigo")).value = "";
+}
+
+function removerAmigo(nomeAmigo){
+    (Amigos.includes(nomeAmigo)) ? (Amigos.splice((Amigos.indexOf(nomeAmigo)), 1)): 0;
+    modificarLista();
 }
 
 function sortear(){
     let listSorteio = document.getElementById("lista-sorteio");
     let sorteado;
-    for (let i = 0; i < Amigos.length; i++) {
-        do {
-            sorteado = Amigos[parseInt(Math.floor(Math.random() * Amigos.length))];
-        } while (Amigos[i] == sorteado || Sorteados.includes(sorteado));
-        
-        Sorteados.push(sorteado);
 
-        listSorteio.innerHTML += `${Amigos[i]} --> ${sorteado}<br>`;
-    }
+    if (Amigos.length < 4) {
+        alert("É preciso um número mínimo de 4 pessoas para realizar o sorteio!");
+    }else{
+        for (let i = 0; i < Amigos.length; i++) {
+            do {
+                sorteado = Amigos[parseInt(Math.floor(Math.random() * Amigos.length))];
+            } while (Amigos[i] == sorteado || Sorteados.includes(sorteado));
+            
+            Sorteados.push(sorteado);
 
-    
+            listSorteio.innerHTML += `${Amigos[i]} --> ${sorteado}<br>`;
+        }
+    } 
+    Sorteados = [];
 }
 
 function reiniciar(){
